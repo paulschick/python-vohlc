52b30c3647946612572eb675fc348efabeb6e985
paulschick <paulschick19@gmail.com>
2021-10-13 09:07:48 -0400

    update setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='volume_ohlc_bars',
+    version='0.0.1',
+    description='Create Candlestick data based on volume instead of time',
+    url='https://github.com/paulschick/python-vohlc',
+    author='Paul Schick',
+    author_email='paul@paulschick.dev',
+    license='MIT',
+    packages=['vohlc'],
+    install_requires=[
+        'ccxt',
+        'pandas'
+    ],
+    zip_safe=True
+)
