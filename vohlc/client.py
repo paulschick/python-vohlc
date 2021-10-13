ccc55d2b6ceb7f43c17cbc2ccb56dfaf712f30b5
paulschick <paulschick19@gmail.com>
2021-10-13 10:17:49 -0400

    Write version 1
    Needs to be tested and then refactored.
    The arguments are very sloppy, but everything chains very nicely.
    Add a few more methods to deal with the persistence cases, rather than smashing it
    into one thing

--- vohlc/client.py
+++ vohlc/client.py
@@ -2,6 +2,7 @@ import os
 import sqlite3 as sql
 import pandas as pd
 from .base.collect_data import create_exchange, run_ohlcvs_collection
+from .base.transform_ohlcv import batch_df
 from typing import List, Dict
 from enum import Enum
 
@@ -12,12 +13,31 @@ class Persistence(Enum):
     SQLITE = 2
 
 
-class Run:
+class Transform:
+    def __init__(self, dataframe: pd.DataFrame, persistence_config: Dict):
+        self.df = dataframe
+        self.pers = persistence_config
+
+    def transform_data(self, batch_size: int) -> pd.DataFrame:
+        df = batch_df(self.df, batch_size)
+        if self.pers['type'].value is Persistence.CSV.value:
+            if self.pers['vohlc_db_path'] is not None:
+                df.to_csv(path_or_buf=self.pers['vohlc_db_path'], index=False)
+            elif self.pers['type'].value is Persistence.SQLITE.value:
+                if self.pers['vohlc_db_path'] is not None:
+                    con = sql.connect(self.pers['vohlc_db_path'])
+                    name = self.pers['sqlite_table2']
+                    df.to_sql(name=name, con=con, index=False)
+                    con.close()
+        return df
+
+
+class Collect:
     def __init__(self, configuration: List, persistance_config: Dict):
         self.config = configuration
         self.pers = persistance_config
 
-    def collect_data(self):
+    def collect_data(self) -> Transform:
         df = run_ohlcvs_collection(*self.config)
         if self.pers['type'].value is Persistence.CSV.value:
             if self.pers['initial_data'] is not None:
@@ -27,6 +47,8 @@ class Run:
                 con = sql.connect(self.pers['vohlc_db_path'])
                 name = self.pers['sqlite_table1']
                 df.to_sql(name=name, con=con, index=False)
+                con.close()
+        return Transform(df, self.pers)
 
 
 class Configuration:
@@ -41,7 +63,7 @@ class Configuration:
         self._table1 = initial_db_table
         self._table2 = vohlc_table
 
-    def configure_client(self, symbol, timeframe, from_iso) -> Run:
+    def configure_client(self, symbol, timeframe, from_iso) -> Collect:
         """Goes directly into run_ohlcvs_collection"""
         self._configuration = [self.exchange, symbol, timeframe, from_iso]
         self._persistence['type'] = self._ptype
@@ -49,4 +71,9 @@ class Configuration:
         self._persistence['vohlc_db_path'] = self.vohlc_path
         self._persistence['sqlite_table1'] = self._table1
         self._persistence['sqlite_table2'] = self._table2
-        return Run(self._configuration, self._persistence)
+        return Collect(self._configuration, self._persistence)
+
+
+# Example Usage
+# df = Configuration('coinbasepro').configure_client('AVAX/USDT', '1h',
+#                                                    '2021-05-01 00:00:00').collect_data().transform_data(1000000)
